5075d5a29feec2796755e83b25d33d950b344670
srbry <srbry@hotmail.com>
2016-06-24 10:18:47 +0100

    Allow extra time to start application due to slower diego start times.

--- chaos-galago-smoke_test.go
+++ chaos-galago-smoke_test.go
@@ -161,15 +161,15 @@ var _ = Describe("Assuming chaos-galago is deployed", func() {
 					Eventually(func() string {
 						output, _ := exec.Command("cf", "curl", fmt.Sprintf("v2/apps/%s/instances", appGUID)).Output()
 						return string(output)
-					}, "120s", "1s").Should(MatchRegexp(`"state": "RUNNING"`))
+					}, "180s", "1s").Should(MatchRegexp(`"state": "RUNNING"`))
 					Eventually(func() string {
 						output, _ := exec.Command("cf", "logs", "chaos-galago-processor", "--recent").Output()
 						return string(output)
-					}, "120s", "1s").Should(MatchRegexp(fmt.Sprintf("About to kill app instance: %s at index: 0", appGUID)))
+					}, "180s", "1s").Should(MatchRegexp(fmt.Sprintf("About to kill app instance: %s at index: 0", appGUID)))
 					Eventually(func() string {
 						output, _ := exec.Command("cf", "curl", fmt.Sprintf("v2/apps/%s/instances", appGUID)).Output()
 						return string(output)
-					}, "120s", "1s").Should(MatchRegexp(`"state": "DOWN"`))
+					}, "180s", "1s").Should(MatchRegexp(`"state": "DOWN"`))
 				})
 			})
 		})
